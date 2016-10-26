c2d139653237e6dae1e98ad6b27c74770a5cb056
sakuracandy <sakuracandy@users.noreply.github.com>
2016-10-26 01:14:02 -0700

    cleanup!

--- 制作刀装.lua
+++ 制作刀装.lua
@@ -1,6 +1,6 @@
 --[[
 模块制作：sakura_candy（肝力不足）
-配方输入（通用锻刀封装库1.33）: uint 
+配方输入（通用锻刀封装库1.33）: uint
 --]]
 
 require("API扩展")
@@ -15,14 +15,14 @@ local 制作刀装 = {}
 --/*      常量定义      */
 --/* ****************** */
 --锻刀正常
-local EQUIP_OK = 1;
-local EQUIP_TEN_OK = 10;
+local EQUIP_OK = 1
+local EQUIP_TEN_OK = 10
 --参数类型错误
-local EQUIP_ERR_INVALID_ARGS_TYPE = -1;
+local EQUIP_ERR_INVALID_ARGS_TYPE = -1
 --参数值错误
-local EQUIP_ERR_INVALID_ARGS_VALUE = -2;
+local EQUIP_ERR_INVALID_ARGS_VALUE = -2
 --异常弹窗
-local EQUIP_ERR_ABNORMAL_MESSAGE_BOX = 0;
+local EQUIP_ERR_ABNORMAL_MESSAGE_BOX = 0
 --是否已经输入好了配方
 local Init = false
 --/**********************/
@@ -34,11 +34,11 @@ local Init = false
 --/**********************/
 function 制作刀装.ProcessMsgbox(option)
     --操作计数器
-    time = 0;
+    time = 0
 
     --并没有对话框，立即返回
     if not 制作刀装.IsMsgbox() then
-        return false;
+        return false
     end
 
     --循环点击直到对话框消失，限时60s（每500ms计数器加一）
@@ -46,21 +46,21 @@ function 制作刀装.ProcessMsgbox(option)
         --是否有"否"按钮
         if Base.ImageHashContrast(Base.ImageHash(536,324,80,30),"FFFFFB7F7B4B0100") < 10 then
                 if option then
-                    Base.ClickRect(386,338);
+                    Base.ClickRect(386,338)
                 else
-                    Base.ClickRect(571,338);
+                    Base.ClickRect(571,338)
                 end
         else
-            Base.ClickRect(486,338);
+            Base.ClickRect(486,338)
         end
-        Base.Sleep(500);
-        time = time + 1;
+        Base.Sleep(500)
+        time = time + 1
     end
 
     if time < 120 then
-        return true;
+        return true
     else
-        return false;
+        return false
     end
 end
 
@@ -71,7 +71,7 @@ end
 --/*  弹出              */
 --/**********************/
 function 制作刀装.IsMsgbox()
-    return (Base.ImageHashContrast(Base.ImageHash(252,153,100,50),"007F7F7F7E7F7F7F") < 10);
+    return (Base.ImageHashContrast(Base.ImageHash(252,153,100,50),"007F7F7F7E7F7F7F") < 10)
 end
 
 --/**********************/
@@ -84,48 +84,48 @@ end
 --/**********************/
 function 制作刀装.EquipMakeInput(operationIndex, materialValue)
     --按钮间距(Y轴)
-    buttonsSpacing = 96;
+    buttonsSpacing = 96
     --百位十位增加按钮X坐标
-    hundredsAndTensAddButtonXPos = 719;
+    hundredsAndTensAddButtonXPos = 719
     --百位十位减少按钮X坐标
-    hundredsAndTensSubButtonXPos = 648;
+    hundredsAndTensSubButtonXPos = 648
     --个位增加按钮X坐标
-    unitsAddButtonXPos = 582;
+    unitsAddButtonXPos = 582
     --百位按钮基础Y坐标
-    hundredsButtonYPosBase = 132;
+    hundredsButtonYPosBase = 132
     --十位按钮基础Y坐标
-    tensButtonYPosBase = 108;
+    tensButtonYPosBase = 108
     --个位增加按钮基础Y坐标
-    unitsAddButtonYPosBase = 111;
+    unitsAddButtonYPosBase = 111
     --单击区域偏移像素个数
-    rectPixels = 2;
+    rectPixels = 2
 
     --拆分数字
-    hundredsDigit = math.floor(materialValue / 100 % 10);
-    tensDigit = math.floor(materialValue / 10 % 10);
-    unitsDigit = math.floor(materialValue % 10);
+    hundredsDigit = math.floor(materialValue / 100 % 10)
+    tensDigit = math.floor(materialValue / 10 % 10)
+    unitsDigit = math.floor(materialValue % 10)
 
     --输入百位
     for i = 1, hundredsDigit do
-        Base.ClickRect(hundredsAndTensAddButtonXPos, hundredsButtonYPosBase + buttonsSpacing * operationIndex, rectPixels);
-        Base.Sleep(500);
+        Base.ClickRect(hundredsAndTensAddButtonXPos, hundredsButtonYPosBase + buttonsSpacing * operationIndex, rectPixels)
+        Base.Sleep(500)
     end
     --输入十位（初始值050因此分类讨论）
     if tensDigit > 5 then
         for i = 1, (tensDigit - 5) do
-            Base.ClickRect(hundredsAndTensAddButtonXPos, tensButtonYPosBase + buttonsSpacing * operationIndex, rectPixels);
-            Base.Sleep(500);
+            Base.ClickRect(hundredsAndTensAddButtonXPos, tensButtonYPosBase + buttonsSpacing * operationIndex, rectPixels)
+            Base.Sleep(500)
         end
     else
         for i = 1, (5 - tensDigit) do
-            Base.ClickRect(hundredsAndTensSubButtonXPos, tensButtonYPosBase + buttonsSpacing * operationIndex, rectPixels);
-            Base.Sleep(500);
+            Base.ClickRect(hundredsAndTensSubButtonXPos, tensButtonYPosBase + buttonsSpacing * operationIndex, rectPixels)
+            Base.Sleep(500)
         end
     end
     --输入个位
     for i = 1, unitsDigit do
-        Base.ClickRect(unitsAddButtonXPos, unitsAddButtonYPosBase + buttonsSpacing * operationIndex, rectPixels);
-        Base.Sleep(500);
+        Base.ClickRect(unitsAddButtonXPos, unitsAddButtonYPosBase + buttonsSpacing * operationIndex, rectPixels)
+        Base.Sleep(500)
     end
 end
 
@@ -142,7 +142,7 @@ end
 function 制作刀装.EquipMakeWithRecipe(charcoal, steel, coolant, grindstone, makeTen)
     --Assume每次执行都是从一个新的任务开始，配方初始化一次就够了
 
-	
+
 
     if not Init then
         --检查传入的参数类型
@@ -153,42 +153,42 @@ function 制作刀装.EquipMakeWithRecipe(charcoal, steel, coolant, grindstone, make
         or (type(makeTen) ~= "boolean")
         then
             Win.Print(type(charcoal)..type(steel)..type(coolant)..type(grindstone)..type(makeTen))
-            return EQUIP_ERR_INVALID_ARGS_TYPE;
+            return EQUIP_ERR_INVALID_ARGS_TYPE
         end
-        
+
         --检查传入的参数范围
         if ((charcoal < 50) or (charcoal > 999))
         or ((steel < 50) or (steel > 999))
         or ((coolant < 50) or (coolant > 999))
         or ((grindstone < 50) or (grindstone > 999))
         then
-            return EQUIP_ERR_INVALID_ARGS_VALUE;
+            return EQUIP_ERR_INVALID_ARGS_VALUE
         end
 
         --按照数值输入
-        制作刀装.EquipMakeInput(0, charcoal);
-        制作刀装.EquipMakeInput(1, steel);
-        制作刀装.EquipMakeInput(2, coolant);
-        制作刀装.EquipMakeInput(3, grindstone);
+        制作刀装.EquipMakeInput(0, charcoal)
+        制作刀装.EquipMakeInput(1, steel)
+        制作刀装.EquipMakeInput(2, coolant)
+        制作刀装.EquipMakeInput(3, grindstone)
         Init = true
     end
 
     --点按刀装制作开始
     if makeTen then
-        Base.ClickRectEx(440,507);
+        Base.ClickRectEx(440,507)
     else
         Base.ClickRectEx(639,506)
     end
-    Base.Sleep(1000);
+    Base.Sleep(1000)
 
     --弹出对话框，可能是满了什么的
     if 制作刀装.IsMsgbox() then
-		制作刀装.ProcessMsgbox(false);
-        return EQUIP_ERR_ABNORMAL_MESSAGE_BOX;
+		制作刀装.ProcessMsgbox(false)
+        return EQUIP_ERR_ABNORMAL_MESSAGE_BOX
     end
-    
+
     取色.等待刀装制作完毕()
-    
+
     if makeTen then
         return EQUIP_TEN_OK
     end
@@ -217,7 +217,7 @@ function 制作刀装.执行(self)
 
         if returncode == 0 and makeTen then
             makeTen = false
-            returncode = 1        
+            returncode = 1
         end
     end
     Win.Print("制作刀装"..Num.."个")
