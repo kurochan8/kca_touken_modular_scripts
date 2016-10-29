18e5a4506b6014e971384a6555d2a5889ef368ee
sakuracandy <sakuracandy@users.noreply.github.com>
2016-10-29 02:38:39 -0700

    加了每x次轮回才执行任务的功能 就是在任务组最后加个parameter，像这样v 最多进入多少次地图, 运行时间上限, {1,5,1,4,1}) 问题是我想不出好的中文parameter name，这种设置方法也很难看...

--- 任务组.lua
+++ 任务组.lua
@@ -22,34 +22,36 @@ function 任务组.开始运行(self)
         Win.Print("任务组：开始执行第" .. tostring(i) .. "次循环")
 
         for j = 1, #self.任务序列, 1 do
-            if type(self.任务序列[j]) == "function" then
-                return_code = self.任务序列[j]()
-            elseif self.任务序列[j]["执行"] and type(self.任务序列[j]["执行"]) == "function" then
-                return_code = self.任务序列[j]["执行"](self.任务序列[j])
-            else
-                Win.Print("任务组：检测到无效任务，中止运行")
-                Win.Print("任务组：" .. require("inspect")(self.任务序列[j]))
-                return -1
+            if math.fmod(i,self.execute_every_x_cycle[j]) == 0 then
+                if type(self.任务序列[j]) == "function" then
+                    return_code = self.任务序列[j]()
+                elseif self.任务序列[j]["执行"] and type(self.任务序列[j]["执行"]) == "function" then
+                    return_code = self.任务序列[j]["执行"](self.任务序列[j])
+                else
+                    Win.Print("任务组：检测到无效任务，中止运行")
+                    Win.Print("任务组：" .. require("inspect")(self.任务序列[j]))
+                    return -1
+                end
+                --如果任何一个任务出错了就直接返回
+                if return_code < 0 then
+                    Win.Print("任务组：任务出错，中止运行，错误代码" .. tostring(return_code))
+                    return return_code
+                end
             end
-            --如果任何一个任务出错了就直接返回
-            if return_code < 0 then
-                Win.Print("任务组：任务出错，中止运行，错误代码" .. tostring(return_code))
-                return return_code
-            end
-
         end
     end
     Win.Print("任务组：到达指定循环次数，结束运行")
     return return_code
 end
 
-function 任务组:new(任务序列, 循环次数, 超时)
+function 任务组:new(任务序列, 循环次数, 超时, execute_every_x_cycle)
     local instance = {}
     setmetatable(instance, {__index = 任务组})
     Base.SetConfig("Sleep+?", 全局设定.操作延时上限)
     instance.任务序列 = 任务序列
     instance.循环次数 = 循环次数 or 2147483647
     instance.超时 = 超时 or 86400
+    instance.execute_every_x_cycle = execute_every_x_cycle
     return instance
 end
 
